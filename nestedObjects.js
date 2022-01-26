1c6c0d2e3371f81107fbce75265f9c144fb08f03
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-01-26 17:12:09 +0300

    add new task

---
+++ nestedObjects.js
@@ -0,0 +1,14 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/accessing-nested-objects
+const myStorage = {
+  car: {
+    inside: {
+      "glove box": "maps",
+      "passenger seat": "crumbs",
+    },
+    outside: {
+      trunk: "jack",
+    },
+  },
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
