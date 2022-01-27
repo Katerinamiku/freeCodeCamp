3edba1d02b9e7dc07a28ef8b60f3fed369f48620
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-01-27 17:09:42 +0300

    add new task

---
+++ doWhile.js
@@ -0,0 +1,9 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-with-javascript-do---while-loops
+
+const myArray = [];
+let i = 10;
+
+do {
+  myArray.push(i);
+  i++;
+} while (i < 11);
