0d3538db388678108e195ca7e995f7225b43138a
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-01-31 14:25:59 +0300

    add new task

---
+++ mathRandom.js
@@ -0,0 +1,11 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/generate-random-fractions-with-javascript
+function randomFraction() {
+  // Only change code below this line
+  let result = 0;
+  while (result === 0) {
+    result = Math.random();
+  }
+  return result;
+
+  // Only change code above this line
+}
