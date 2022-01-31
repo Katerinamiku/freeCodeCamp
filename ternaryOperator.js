04f6df6d9407f189b630b5a10c6a10a854bfc7e7
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-01-31 17:19:08 +0300

    add new task

---
+++ ternaryOperator.js
@@ -0,0 +1,4 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-multiple-conditional-ternary-operators
+function checkSign(num) {
+  return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+}
